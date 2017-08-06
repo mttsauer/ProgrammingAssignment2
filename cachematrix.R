95b4b51193f43508841cd033edf1c32e1bb50a73
Matt Sauer <matt.sauer@gmail.com>
2017-08-06 15:07:32 -0500

    Completion of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are intended to work together to 
+## eliminate redundant calls to the solve function.  This is accomplished
+## by using a cache.
 
-## Write a short comment describing this function
+## Constructs an object capable of storing a matrix and a cached value. 
+## 
+## x should be of type matrix
+## returns a list of functions:
+##    get: returns x
+##    setcache: allowing caller to set the cache value
+##    getcache: allowing caller to access the cache value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   c <- NULL
+   set <- function(y) {
+      x <<- y
+      c <<- NULL
+   }
+   get <- function() x
+   setcache <- function(cache) c <<- cache
+   getcache <- function() c
+   list(set = set, get = get,
+        setcache = setcache,
+        getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## Finds the inverse of x using the solve function.  If solve has already been 
+## calculated on x, a cached value is returned.
+##
+## x must be instance of makeCacheMatrix
+## ... arguments will be passed through to the solve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+   if(class(x) == "matrix") {
+      stop("atomic vectors or matrix not valid.  Use makeCacheMatrix to construct valid matrix")
+   }
+   
+   c <- x$getcache()
+   if(!is.null(c)) {
+      message("getting cached data")
+      return(c)
+   }
+   data <- x$get()
+   c <- solve(data, ...)
+   x$setcache(c)
+   c
 }
